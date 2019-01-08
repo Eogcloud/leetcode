cd0b082c1f2c8c9d4b809de70dd3993b07105464
I310538 <eamonn.o.broithe@sap.com>
2019-01-08 14:49:01 +0000

    Started 34.

---
+++ src/34. Find First and Last Position of Element in Sorted Array/lc34.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+ let output = []
+	if( output.length > 2 ){
+		return output
+	}
+	else{
+		if( target > nums[ Math.floor( nums.length / 2)]){
+			searchRange( nums.slice( Math.floor( nums.length / 2 ))
+		}
+	}
+}
